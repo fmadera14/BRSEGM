5402ffcc0f3106a4feb431b1df7afc24df16c8eb
Francisco Madera <franciscomaderanatera@gmail.com>
2025-12-24 08:25:26 -0400

    fix: parametro dto

--- ConfiguracioParametros/Data/DTOs/ParametroDto.cs
+++ ConfiguracioParametros/Data/DTOs/ParametroDto.cs
@@ -3,11 +3,14 @@
     public class ParametroDto
     {
         public int IdParametro { get; set; }
-        public string NombreClave { get; set; }
-        public string Descripcion { get; set; }
-        public string Valor { get; set; }
+        public string NombreClave { get; set; } = string.Empty;
+        public string Descripcion { get; set; } = string.Empty;
+        public string Valor { get; set; } = string.Empty;
 
         public int IdEstado { get; set; }
-        public string Estado { get; set; }
+        public string Estado { get; set; } = string.Empty;
+        
+        public int IdTipoParametro { get; set; }
+        public string TipoParametro { get; set; } = string.Empty;
     }
 }
